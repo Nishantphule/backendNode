2fa99dda9e58507113d1c5d433e697119dc61098
Nishantphule <nishantphule12@gmail.com>
2023-07-01 10:06:02 +0530

    node_module

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get('/', (req,res) => {
    res.send("<h1>Hello World</h1>") 
 });
 
+app.get('/api/notes', (req,res) => {
+    res.json(notes) 
+ });
+
 const PORT = 3001;
 app.listen(PORT , () =>{
     console.log(`server running on port ${PORT}`);
