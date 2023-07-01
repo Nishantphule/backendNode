aabe567c995cbdcdac35b711bd0c7cc2bb7fb3bf
Nishantphule <nishantphule12@gmail.com>
2023-07-01 09:28:38 +0530

    backend-node-server1.1

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 const http = require("http");
 
-let notes = [
-    {
-        id:1,
-        content:"HTML is easy",
-        important:true
-    },
-    {
-        id:2,
-        content:"Browser can only execute js",
-        important:false
-    },
-    {
-        id:3,
-        content:"GET and POST are most important methods of HTTP protocol",
-        important:true
-    },
-]
-
 const app = http.createServer((req,res) => {
     res.writeHead(200,{
-        "Content-Type": 'application/json'
+        "Content-Type": 'text/plain'
         });
-    res.end(JSON.stringify(notes));    
+    res.end("Hello World");    
 })
 
 const PORT = 3001;
+
 app.listen(PORT);
 console.log(`server running on port ${PORT}`);
 
