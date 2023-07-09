75d404b85e7f3ed2ddbab9f65722dc6cf5b0cff4
Nishantphule <nishantphule12@gmail.com>
2023-07-09 08:23:58 +0530

    crud

--- index.js
+++ index.js
@@ -1,162 +1,128 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 app.use(express.json());
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can only execute js",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are most important methods of HTTP protocol",
-        important: true
-    },
-]
-
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
-
-
-
-// set the endpoints for notes
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
-});
-
-// get all notes
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-});
-
-//  add note
-app.post('/api/addnote', (req, res) => {
-    notes = notes.concat(req.body)
-    res.status(201).json({ messsage: "note created successfully" })
-});
-
-// get by id
-app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end("Id does not exist")
-    }
-})
-
-// delete
-app.delete("/api/deletenote/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(note => note.id === id);
-    notes = notes.filter(note => note.id !== id);
-    res.status(204).end("note deleted successfully");
-})
-
-// put
-app.put('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    // const note = notes.find(note => note.id === id);
-
-    const notToPut = req.body
-
-    notes = notes.map(note => note.id === id ? notToPut : note)
-
-    res.status(200).end(`Note with Id ${id} updated succesfully`)
-})
-
-
-
-// set the endpoints for persons
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
-});
-
-// get all notes
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
-});
-
-//  add note
-app.post('/api/addperson', (req, res) => {
-    persons = persons.concat(req.body)
-    res.status(201).json({ messsage: "person created successfully" })
-});
-
-// get by id
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end("Id does not exist")
-    }
-})
-
-// delete
-app.delete("/api/deleteperson/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find(person => person.id === id);
-    persons = persons.filter(person => person.id !== id);
-    res.status(204).end("note deleted successfully");
-})
-
-// put
-app.put('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    // const note = notes.find(note => note.id === id);
-
-    const personToPut = req.body
-
-    persons = persons.map(person => person.id === id ? personToPut : person)
-
-    res.status(200).end(`Person with Id ${id} updated succesfully`)
-})
-
-// info
-app.get('/info', (req, res) => {
-
-    const noOfPersons = persons.length
+  
     
-    const timeInfo = new Date();
+const URL = `mongodb+srv://nishantphule:Nishant08@cluster0.sorol20.mongodb.net/?retryWrites=true&w=majority`
 
-    res.send(`
-    <h1>Phonebook has Info for ${noOfPersons} people</h1> 
-    <p>${timeInfo}</p>
-    `)
+// set the strictQuery to false, so that it will disable the strict mode for the query filters
+// mongoose will not throw any error when we use an undefined field in the query (ignored)
+mongoose.set('strictQuery', false);
+
+// to connect to the database
+mongoose.connect(URL);
+
+// optional: to check whether a successful connection is made to the mongoDB database
+const db = mongoose.connection;
+
+db.once('connected', () => {
+    console.log('Connected to MongoDB Database');
+    // mongoose.connection.close();
+})
+db.on('error', console.error.bind(console, 'Connection Error'));
+
+// create a schema
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    important: Boolean
+});
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
+// create a model
+const Note = mongoose.model('Note', noteSchema, 'notes');
+
+// set the endpoints
+
+// root end point: prints hello world as an HTML
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World!</h1>');
+// });
+
+// fetches all resources in the collection
+app.get('/api/notes', (request, response) => {
+    Note.find({}, {})
+    .then((notes) => {
+        response.json(notes);
+    });
+});
+
+// creates a new resource based on the request data
+app.post('/api/notes', (request, response) => {
+    // prepare an object to store it in the database
+    const note = new Note(request.body);
+
+    // storing the new object to the database
+    note.save()
+        .then(result => {
+            response.status(201).json({ message: 'Note created successfully' });
+        });
+});
+
+// fetching a sigle resource
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    Note.findById(id)
+        .then((note) => {
+            if (!note) {
+                return response.status(404).json({ error: 'Note not found' });
+            }
+            response.json(note);
+        })
+        .catch((error) => {
+            response.status(500).json({ error: 'Internal server error' });
+        });
+});
+
+// deleting a resource
+app.delete('/api/notes/:id', (request, response) => {
+    // get the id of the resource from params
+    const id = request.params.id;
+
+    Note.findByIdAndDelete(id)
+        .then((deletedNote) => {
+            if (!deletedNote) {
+                return response.status(404).json({ error: 'Note not found' });
+            }
+            response.status(204).json({ message: 'Note deleted successfully' });
+        })
+        .catch((error) => {
+            response.status(500).json({ error: 'Internal server error' });
+        });
+
+    // response.status(204).end();
+});
+
+// replace the entire identified resource with the request data
+app.put('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    const noteToPut = request.body;
+
+    Note.findByIdAndUpdate(id, noteToPut)
+        .then((updatedNote) => {
+            if (!updatedNote) {
+                return response.status(404).json({ error: 'Note not found' });
+            }
+            response.json(updatedNote);
+        })
+        .catch((error) => {
+            response.status(500).json({ error: 'Internal server error' });
+        });
+});
+
+// Listen to the PORT for requests
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
-
-
